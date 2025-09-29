7d718a606290957944598af62c55fec546697cf8
davidVeer <davidvdveer05@gmail.com>
2025-09-29 14:56:44 +0200

    adding message echo to all clients

--- PI introactiviteit Server/SimpleServer.cs
+++ PI introactiviteit Server/SimpleServer.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PI_introactiviteit_Server
 {
@@ -13,8 +9,7 @@ namespace PI_introactiviteit_Server
         private TcpListener listener;
         private List<TcpClient> clients = new List<TcpClient>(); // Keeps track of connected clients
 
-        public void Start(int port)
-        {
+        public void Start(int port) {
             listener = new TcpListener(IPAddress.Any, port);
             listener.Start();
             Console.WriteLine("Server started, waiting for connections...");
@@ -23,55 +18,50 @@ namespace PI_introactiviteit_Server
             acceptThread.Start();
         }
 
-        private void AcceptClients()
-        {
-            while (true)
-            {
-                try
-                {
+        private void AcceptClients() {
+            while (true) {
+                try {
                     TcpClient client = listener.AcceptTcpClient();
                     clients.Add(client);
                     Console.WriteLine("Client connected.");
 
                     Thread clientThread = new Thread(() => HandleClient(client));
                     clientThread.Start();
-                }
-                catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     Console.WriteLine("Error accepting client: " + ex.Message);
                 }
             }
         }
 
-        private void HandleClient(TcpClient client)
-        {
+        private void HandleClient(TcpClient client) {
             NetworkStream stream = client.GetStream();
             byte[] buffer = new byte[1024];
             int bytesRead;
 
-            try
-            {
-                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-                {
+            try {
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0) {
                     string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     Console.WriteLine("Received: " + message);
 
-                    // Echo message back to the client
-                    byte[] response = Encoding.UTF8.GetBytes("Echo: " + message);
-                    stream.Write(response, 0, response.Length);
+                    SendMessageToAllClients(message);
                 }
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 Console.WriteLine("Error communicating with client: " + ex.Message);
-            }
-            finally
-            {
+            } finally {
                 stream.Close();
                 client.Close();
                 clients.Remove(client);
                 Console.WriteLine("Client disconnected.");
             }
+        }
+
+        private void SendMessageToAllClients(string message) {
+            byte[] response = Encoding.UTF8.GetBytes(message);
+            foreach (var client in clients) {
+                NetworkStream stream = client.GetStream();
+                stream.Write(response, 0, response.Length);
+            }
+
         }
     }
 }
